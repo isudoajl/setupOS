c0c66c8a0201678b55798621be33ed6d500a3c37
isudoajl <ajlozadar@gmail.com>
2023-02-10 23:24:23 +0000

    update

--- fstab.sh
+++ fstab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "*** CREATING sda1 AND sda2 DIRECTORIES in /mnt ***"
-if ! sudo mkdir /mnt/sda1 /mnt/sda2; then
+echo "*** CREATING sda1 sda2 and old-sda2 DIRECTORIES in /mnt ***"
+if ! sudo mkdir /mnt/sda1 /mnt/sda2 /mnt/old-sda2; then
   echo "Error: Failed to create directories"
   exit 1
 fi
@@ -12,6 +12,7 @@ echo "*** MOUNTING sda1 AND sda2 DEVICES ***"
 
 sda1_uuid="dae51dd4-c895-4730-b153-156b394da23f"
 sda2_uuid="eec760a8-a536-4f5d-9931-c39c6e4dab0c"
+sda3_uuid="8592534b-b3ba-42f7-a344-51b11208ab88"
 
 if ! echo "UUID=$sda1_uuid /mnt/sda1 ext4 defaults 0 0" | sudo tee -a /etc/fstab > /dev/null; then
   echo "Error: Failed to add sda1 to /etc/fstab"
@@ -23,6 +24,12 @@ if ! echo "UUID=$sda2_uuid /mnt/sda2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
   exit 1
 fi
 
+if ! echo "UUID=$sda2_uuid /mnt/old-sda2 ext4 defaults 0 0" | sudo tee -a /etc/fstab > /dev/null; then
+  echo "Error: Failed to add old-sda2 to /etc/fstab"
+  exit 1
+fi
+
+
 if ! sudo mount /mnt/sda1; then
   echo "Error: Failed to mount sda1"
   exit 1
@@ -32,6 +39,12 @@ if ! sudo mount /mnt/sda2; then
   echo "Error: Failed to mount sda2"
   exit 1
 fi
+
+if ! sudo mount /mnt/old-sda2; then
+  echo "Error: Failed to mount old-sda2"
+  exit 1
+fi
+
 
 if ! sudo systemctl daemon-reload; then
   echo "Error: Failed to reload system daemons"
--- homelinks.sh
+++ homelinks.sh
@@ -2,7 +2,7 @@
 echo "*** REMOVE DIRECTORYS ***"
 directories=(Documents Pictures Music Videos Templates Public Desktop Downloads)
 for dir in "${directories[@]}"; do
-  rm -fr ~/ "$dir"
+  rm -fr ~/"$dir"
 done
 
 echo "*** CREATE SOFT-LINKS ***"
