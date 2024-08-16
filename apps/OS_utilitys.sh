5bb51b8c9155151f496d75b270f14e4cb68499f7
isudoajl <isudosap@gmail.com>
2024-08-16 21:57:40 +0100

    xxxxxx

--- apps/OS_utilitys.sh
+++ apps/OS_utilitys.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "*** INSTALL SOME UTILITYS ***"
-sudo dnf xclip install gnome-tweaks htop inxi powertop neofetch vim gkrellm snapd tlp cronie libappindicator-gtk3 gnome-shell-extension-appindicator rsyslog fdupes podman-compose setroubleshoot-server -y
+sudo dnf install xclip gnome-tweaks htop inxi powertop neofetch vim gkrellm snapd tlp cronie libappindicator-gtk3 gnome-shell-extension-appindicator rsyslog fdupes podman-compose setroubleshoot-server -y
 if [ $? -ne 0 ]; then
   echo "ERROR: Failed to install utilities."
   exit 1
