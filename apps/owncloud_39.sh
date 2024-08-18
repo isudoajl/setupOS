3f072526aa93d001ea7d02fecabfd496175cd633
isudovm <isudosap@gmail.com>
2024-08-18 16:38:15 +0100

    xxxx

--- apps/owncloud_39.sh
+++ apps/owncloud_39.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 grep 'Fedora 39' /etc/os-release || ( echo "This INSTALL.sh is for 'Fedora 39'. Press CTRL-C to abort, ENTER to continue"; read a )
-dnf -y install 'dnf-command(config-manager)'
+sudo dnf -y install 'dnf-command(config-manager)'
 rpm --import https://download.owncloud.com/desktop/ownCloud/stable/latest/linux/Fedora_39/repodata/repomd.xml.key
-dnf config-manager --add-repo https://download.owncloud.com/desktop/ownCloud/stable/latest/linux/Fedora_39/owncloud-client.repo || exit 1
-dnf clean all
-dnf install owncloud-client -y
-dnf upgrade owncloud-client -y
+sudo dnf config-manager --add-repo https://download.owncloud.com/desktop/ownCloud/stable/latest/linux/Fedora_39/owncloud-client.repo || exit 1
+sudo dnf clean all
+sudo dnf install owncloud-client -y
+sudo dnf upgrade owncloud-client -y
